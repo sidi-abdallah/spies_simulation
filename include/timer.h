567219651f47a1a3cad7afe169a5010ddf54b729
Youssef AGHZERE <aghzerey@gmail.com>
2022-11-23 17:02:50 +0100

    premier seance timer

---
+++ include/timer.h
@@ -0,0 +1,9 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#define ROUND_NUMBER 2016 
+ 
+
+
+
+#endif /* TIMER_H */
---
+++ src/timer.c
@@ -0,0 +1,14 @@
+#include "timer.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+
+void timer_handler(int i){
+    unsigned int i;
+    
+}
+
+int main(int argc, char * argv[]){
+    
+}
